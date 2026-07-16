01d867adc2e6386196aee66634d7e40c9697ea7e
laomou <rui.mou@foxmail.com>
2026-07-16 19:07:15 +0800

    fix: localize skip navigation label

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import AppNotice from '@/components/AppNotice.vue'
 import DownloadStatus from '@/components/DownloadStatus.vue'
 import MiniPlayer from '@/components/MiniPlayer.vue'
 import { usePlayerShortcuts } from '@/composables/usePlayerShortcuts'
+import { t } from '@/i18n'
 
 const route = useRoute()
 const isConnect = computed(() => route.path === '/connect')
@@ -14,7 +15,7 @@ usePlayerShortcuts()
 
 <template>
   <AudioPlayer />
-  <a class="skip-link" href="#main-content">Skip to content</a>
+  <a class="skip-link" href="#main-content">{{ t('common.skipToContent') }}</a>
   <main id="main-content" class="app-shell" :class="{ 'connect-shell': isConnect }" tabindex="-1">
     <RouterView />
   </main>
