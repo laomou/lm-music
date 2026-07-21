89f2ba563c80500f98975270cdd30dd3500d2041
laomou <rui.mou@foxmail.com>
2026-07-21 17:24:27 +0800

    refactor: extract base64url helpers

--- src/services/local/client.ts
+++ src/services/local/client.ts
@@ -1,5 +1,6 @@
 import { t } from '@/i18n'
 import type { LocalFolderSession, LyricLine, Playlist, Track } from '@/types/music'
+import { decodeBase64Url, encodeBase64Url } from '@/utils/base64Url'
 import { parseLrc } from '@/utils/parseLrc'
 
 const DB_NAME = 'lm-music-local-folder'
@@ -12,19 +13,6 @@ type LocalTrackReference = {
   path: string[]
   lastModified: number
   size: number
-}
-
-function encodeBase64Url(value: string) {
-  const bytes = new TextEncoder().encode(value)
-  let binary = ''
-  for (const byte of bytes) binary += String.fromCharCode(byte)
-  return btoa(binary).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
-}
-
-function decodeBase64Url(value: string) {
-  const base64 = value.replace(/-/g, '+').replace(/_/g, '/').padEnd(Math.ceil(value.length / 4) * 4, '=')
-  const binary = atob(base64)
-  return new TextDecoder().decode(Uint8Array.from(binary, (character) => character.charCodeAt(0)))
 }
 
 export function encodeLocalTrackId(reference: LocalTrackReference) {
